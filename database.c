767dd08737d881ef1e4cd76228b43a8af40877cc
cyr <kirstar13@yandex.ru>
2021-03-12 02:54:12 +0300

    fixes

--- database.c
+++ database.c
@@ -75,7 +75,7 @@ typedef struct {
     Pager* pager;
 } Table;
 
-+typedef struct {
+typedef struct {
     Table* table;
     uint32_t row_num;
     bool end_of_table;  // Indicates a position one past the last element
@@ -321,24 +321,29 @@ PrepareResult prepare_statement(InputBuffer* input_buffer,
 }
 
 ExecuteResult execute_insert(Statement* statement, Table* table) {
-  if (table->num_rows >=TABLE_MAX_ROWS) {
-    return EXECUTE_TABLE_FULL;
-  }
+    Row* row_to_insert = &(statement->row_to_insert);
+    Cursor* cursor = table_end(table);
 
-  Row* row_to_insert = &(statement->row_to_insert);
-  serialize_row(row_to_insert, row_slot(table, table->num_rows));
-  table->num_rows += 1;
+    serialize_row(row_to_insert, cursor_value(cursor));
+    table->num_rows += 1;
 
-  return EXECUTE_SUCCESS;
+    free(cursor);
+
+   return EXECUTE_SUCCESS;
 }
 
-ExecuteResult execute_select(Statement* statement, Table* table) {
-  Row row;
-  for (uint32_t i = 0; i < table->num_rows; i++) {
-    deserialize_row(row_slot(table, i), &row);
-    print_row(&row);
-  }
-  return EXECUTE_SUCCESS;
+
+ ExecuteResult execute_select(Statement* statement, Table* table) {
+    Cursor* cursor = table_start(table);
+    Row row;
+    while (!(cursor->end_of_table)) {
+        deserialize_row(cursor_value(cursor), &row);
+        print_row(&row);
+        cursor_advance(cursor);
+    }
+    free(cursor);
+
+    return EXECUTE_SUCCESS;
 }
 
 ExecuteResult execute_statement(Statement* statement, Table* table) {
